9299b6d4781eabe88a416e06e978d11a595bf64d
daniel <danielboydelatour@gmail.com>
2015-03-11 10:35:16 -0400

    add file extension to default sql script

--- vim/run_sql_client.py
+++ vim/run_sql_client.py
@@ -3,5 +3,5 @@ import os, sys
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 else:
-    filename = "editor"
+    filename = "editor.sql"
 os.system('vim -c ":source sql_client.vim" -o ' + filename + ' viewer')
